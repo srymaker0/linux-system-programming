e33de9f07d50492b73f084b374ecfb5ee8e8acab
srymaker0 <zyjay0@gmail.com>
2021-10-02 17:44:56 +0800

    scanfdir.c

--- 03.Ls/scanfdir.c
+++ 03.Ls/scanfdir.c
@@ -6,27 +6,25 @@
  ************************************************************************/
 
 #define _DEFAULT_SOURCE
-       #include <dirent.h>
-       #include <stdio.h>
-       #include <stdlib.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-       int
-       main(void)
-       {
-           struct dirent **namelist;
-           int n;
+int main(void) {
+   struct dirent **namelist;
+   int n;
 
-           n = scandir(".", &namelist, NULL, alphasort);
-           if (n == -1) {
-               perror("scandir");
-               exit(EXIT_FAILURE);
-           }
+   n = scandir(".", &namelist, NULL, alphasort);
+   if (n == -1) {
+       perror("scandir");
+       exit(EXIT_FAILURE);
+   }
 
-           while (n--) {
-               printf("%s\n", namelist[n]->d_name);
-               free(namelist[n]);
-           }
-           free(namelist);
+   while (n--) {
+       printf("%s\n", namelist[n]->d_name);
+       free(namelist[n]);
+   }
+   free(namelist);
 
-           exit(EXIT_SUCCESS);
-       }
+   exit(EXIT_SUCCESS);
+}
